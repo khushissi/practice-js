bbff9972c3f61debba7a934d6566da766ceac061
khushi <khushibansal@gmail.com>
2023-11-16 15:23:51 +0530

    thapa

---
+++ if else.jsx
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <script>
+      var res;
+      var a=prompt("enter the value of a") ;
+      res=(a>=0) ? "positive" : "negative";
+      Document.write("value of res",+ res);
+    </script>
+</body>
+</html>
